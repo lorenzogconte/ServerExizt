f54ea07901dfe24adf051430de773748478fdda9
Lorenzo <lorenzo.gabriel.conte@estudiantat.upc.edu>
2025-04-24 18:34:15 +0200

    First commit

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.username
+
+class Profile(models.Model):
+    username = models.CharField(max_length = 20)
+    
